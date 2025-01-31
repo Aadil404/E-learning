11783bc17d8c73ddf40f726a42a53b125157f3d0
Aadil404 <mdadilsss2003@gmail.com>
2025-02-01 00:24:56 +0530

    add loading screen when loadUser isLoading is true

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { appStore } from './app/store'
-import { Toaster } from "@/components/ui/sonner"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { appStore } from "./app/store";
+import { Toaster } from "@/components/ui/sonner";
+import LoadingSpinner from "./components/LoadingSpinner";
+import { useLoadUserQuery } from "./features/api/authApi";
 
-createRoot(document.getElementById('root')).render(
+const Custom = (props) => {
+  const { isLoading } = useLoadUserQuery();
+  return <div>{isLoading ? <LoadingSpinner /> : props.children}</div>;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-      <App />
-      <Toaster />
+      <Custom>
+        <App />
+        <Toaster />
+      </Custom>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
