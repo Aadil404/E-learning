ae8ab4ef18ee6e49d43a7180624edaa343866e7f
Aadil404 <mdadilsss2003@gmail.com>
2025-02-16 23:46:52 +0530

    feat: add video lecture upload functionality with progress indicator in LectureTab component

--- frontend/src/pages/admin/lecture/LectureTab.jsx
+++ frontend/src/pages/admin/lecture/LectureTab.jsx
@@ -8,14 +8,57 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Progress } from "@/components/ui/progress";
 import { Switch } from "@/components/ui/switch";
+import axios from "axios";
 import { Loader } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const MEDIA_API = "http://localhost:8080/api/media";
 
 const LectureTab = () => {
   const navigate = useNavigate();
   const isLoading = false;
+
+  const [title, setTitle] = useState("");
+  const [uploadVideoInfo, setUploadVideoInfo] = useState(null);
+  const [isFree, setIsFree] = useState(false);
+  const [mediaProgress, setMediaProgress] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  const fileChangeHandler = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      setMediaProgress(true);
+      try {
+        const res = await axios.post(`${MEDIA_API}/upload-video`, formData, {
+          onUploadProgress: ({ loaded, total }) => {
+            setUploadProgress(Math.round((loaded / total) * 100));
+          },
+        });
+
+        if (res.data.sucess) {
+          setUploadVideoInfo({
+            videoUrl: res.data.data.url,
+            publicId: res.data.data.public_id,
+          });
+          setBtnDisabled(false);
+          toast.success("Video uploaded successfully");
+        }
+      } catch (error) {
+        console.log("error in uploading video", error);
+        toast.error("Error in uploading video");
+      } finally {
+        setMediaProgress(false);
+      }
+    }
+  };
+
   return (
     <Card>
       <CardHeader className="flex flex-row justify-between">
@@ -48,8 +91,20 @@ const LectureTab = () => {
             <Label>
               Upload Video<span className="text-red-600">*</span>
             </Label>
-            <Input type="file" accept="video/*" name="videoUrl" />
+            <Input
+              type="file"
+              onChange={fileChangeHandler}
+              accept="video/*"
+              name="videoUrl"
+            />
           </div>
+
+          {mediaProgress && (
+            <div className="my-4">
+              <Progress value={uploadProgress} className="w-full" />
+              <p>{uploadProgress}%</p>
+            </div>
+          )}
 
           <div className="flex items-center space-x-2">
             <Label>Free Preview</Label>
