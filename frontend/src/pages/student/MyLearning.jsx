4c70e34b6076817454cfa41498691dd54a1da330
Md Adil <mdadilsss2003@gmail.com>
2025-08-10 17:11:01 +0530

    feat: add course in enrolled courses and my learning

--- frontend/src/pages/student/MyLearning.jsx
+++ frontend/src/pages/student/MyLearning.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Course from "./Course";
+import { useLoadUserQuery } from "@/features/api/authApi";
 
 const MyLearning = () => {
-  const isLoading = false;
-  const myLearningCourses = [1, 2];
+  const {isLoading: isLoading,
+    data: data, isSuccess} = useLoadUserQuery();
+
+    const [myLearningCourses, setMyLearningCourses] = useState([]);
+    useEffect(() => {
+      console.log("Data loaded:", data);
+      if (isSuccess) {
+        setMyLearningCourses(data?.user?.enrolledCourses || []);
+      }
+    }, [isSuccess, data]);
+  
   return (
     <div className="max-w-4xl mx-auto my-24 px-4 md:px-0">
       <h1 className="font-bold text-2xl">MY LEARNING</h1>
@@ -15,7 +25,7 @@ const MyLearning = () => {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {myLearningCourses.map((course, index) => (
-              <Course key={index} />
+              <Course key={index} course={course} />
             ))}
           </div>
         )}
